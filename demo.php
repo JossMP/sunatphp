48d1ad051e3b995640055f43c12152f53108d9a4
Josue Mazco Puma <jossmp@gmail.com>
2017-03-07 00:55:50 -0500

    update 07/03/2017 - add sample

--- demo.php
+++ demo.php
@@ -3,7 +3,7 @@
     require ("sunat.php");
     $cliente = new Sunat();
     $ruc="20552103816";
-    $dni="44274795";
+    $dni="XXXXXXXX";
     header('Content-Type: application/json');
     echo "---------RUC----------\n";
     echo json_encode( $cliente->BuscaDatosSunat($ruc), JSON_PRETTY_PRINT );
